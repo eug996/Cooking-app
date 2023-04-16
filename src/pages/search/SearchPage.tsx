cb87d2b4f01195e0afcd67dbaa2a85067883834a
eug996 <104517572+eug996@users.noreply.github.com>
2023-04-16 17:16:58 +0100

    search page added

--- src/pages/search/SearchPage.tsx
+++ src/pages/search/SearchPage.tsx
@@ -1,7 +1,41 @@
-// src/pages/search/SearchPage.tsx
-  
+import RecipeList, { type Recipe,} from "../../components/RecipeList/RecipeList";
+import { useFetch } from "../../hooks/useFetch";
+import { useSearchParams } from "react-router-dom";
+
 import "./SearchPage.css";
-  
+
 export default function SearchPage() {
-  return <h1>Search</h1>;
+
+
+  let [searchParams] = useSearchParams();
+  const query = searchParams.get("q");
+
+  const { data, error } = useFetch<Recipe[]>(`${process.env.REACT_APP_DB_URL}?q=${query}`);
+
+
+
+
+
+  if (error)
+    return (
+      <p aria-live="polite" role="status">
+        {error.message}
+      </p>
+    );
+  if (!data)
+    return (
+      <p aria-live="polite" role="status">
+        Loading...
+      </p>
+    );
+
+  return  (
+  <>
+  <RecipeList recipes={data} />
+  <h2>Recipes including "{query}"</h2>
+  </>
+  );
+
+
+  
 }
